a0b11fa1e009c73a5790879f81b27370ffafd26c
buldoizer <vladvr2011@yandex.ru>
2023-08-29 15:19:40 +0500

    Рабочая кнопка

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ new Swiper(swiper, {
 
 // Button
 
+const clubBtnContainer = document.querySelector(".part-of-club-btn")
 const clubBtn = document.querySelector(".part-of-club-btn button")
+const main = document.querySelector('main')
 let maxOffset = clubBtn.getBoundingClientRect().bottom
 
 
 document.addEventListener("scroll", () => {
-    console.log(clubBtn.getBoundingClientRect().bottom, maxOffset)
-    if(clubBtn.getBoundingClientRect().bottom < maxOffset) {
+    console.log(window.scrollY + window.innerHeight, window.innerHeight)
+    if(window.scrollY + window.innerHeight >= 3060) {
         clubBtn.textContent = "Хочу абонемент"
-        clubBtn.classList.add("part-of-club-btn-static")
+        clubBtnContainer.classList.add("part-of-club-btn-static")
+        main.classList.remove("margin-bottom")
     } else {
         maxOffset = clubBtn.getBoundingClientRect().bottom
         clubBtn.textContent = "Стать частью клуба"
-        clubBtn.classList.remove("part-of-club-btn-static")
+        clubBtnContainer.classList.remove("part-of-club-btn-static")
+        main.classList.add("margin-bottom")
     }
 })
